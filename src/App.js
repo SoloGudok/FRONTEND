f13b5a705f6b2908859806493006e621cdf879bf
Minkyuuu <145241803+Minkyuuu@users.noreply.github.com>
2025-03-04 16:58:13 +0900

    Create Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <h1>모바일 전용 페이지</h1>
+      <p>이 웹사이트는 모바일에서만 최적화되었습니다.</p>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import logo from "./img/logo.png";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div id="header">
+        <div id="header-logo-image">
+          <a href="#">
+            <img src={logo} />
+          </a>
+        </div>
+
+        <div id="header-menus">
+          <a href="#">
+            <div id="header-menu1">구독 서비스</div>
+          </a>
+
+          <a href="#">
+            <div id="header-menu2">카드</div>
+          </a>
+
+          <a href="#">
+            <div id="header-menu3">마이</div>
+          </a>
+
+          <a href="#">
+            <div id="header-menu4">혜택</div>
+          </a>
+
+          <a href="#">
+            <div id="header-menu5">해지</div>
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Header;
