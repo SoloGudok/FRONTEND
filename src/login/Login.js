841752e058d7f39aa26054a22c12e90f56c5eee0
k2xh <k2xh0115@gmail.com>
2025-03-20 11:27:49 +0900

    로그인창 수정중

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import "./Login.css";
 
 const Login = ({ onLogin }) => {
     const [email, setEmail] = useState("");
@@ -23,13 +24,23 @@ const Login = ({ onLogin }) => {
             alert("로그인 실패");
         }
     };
-
     return (
-        <div>
-            <h2>로그인</h2>
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="이메일 입력" />
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="비밀번호 입력" />
-            <button onClick={handleLogin}>로그인</button>
+        <div className="login-container">
+            <div className="login-box">
+                <h2 className="login-title">로그인</h2>
+
+                <div className="input-group">
+                    <input  type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="이메일 입력" />
+                    <button className="clear-btn">×</button>
+                </div>
+
+                <div className="input-group">
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="비밀번호 입력" />
+                    <button className="clear-btn">×</button>
+                </div>
+
+                <button className="login-btn" onClick={handleLogin}>로그인 하기</button>
+            </div>
         </div>
     );
 };
